a62a105f227e08ea86861ac830dc71c5b87b6985
Mark Gray <mfgray87@gmail.com>
2025-09-09 21:17:07 +0100

    fixing migrations

---
+++ alembic/versions/e21c8b11ba88_merge_goal_migrations.py
@@ -0,0 +1,13 @@
+revision = 'e21c8b11ba88'
+down_revision = ('9c3a5d2e3f6a', 'a1f2c3d4e5f6')
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
